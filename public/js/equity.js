6e458383a855e0ff81e852261ef9cdaa8d42f4a9
liyang0503 <liyang0503@126.com>
2017-07-20 18:09:22 +0800

    金融

---
+++ public/js/equity.js
@@ -0,0 +1,41 @@
+/**
+ * Created by QXT on 2017/7/7.
+ */
+
+$(function () {
+   //   精品路演
+   $('.qua-num').each(function () {
+      var s1 = $(this).find('.s1').text();
+      var s2 = $(this).find('.s2').text();
+      var num = parseInt((s2 / s1) * 100);
+      $(this).find('.progress-bar').css({width: num + "%"});
+   });
+   //   最新上线
+   $('.newest-num').each(function () {
+      var s1 = $(this).find('.s1').text();
+      $(this).find('.progress-bar').css({width: s1});
+   });
+
+   //   手机头部下拉
+   $('.but-ico').click(function () {
+      $(this).next('.secs').slideToggle('fast');
+   });
+
+});
+
+//手机搜索
+function searchToggle(obj, e){
+   var container = $(obj).closest('.search-wrapper');
+
+   if(!container.hasClass('active')){
+      container.addClass('active');
+      e.preventDefault();
+   }
+   else if(container.hasClass('active') && $(obj).closest('.input-holder').length == 0){
+      container.removeClass('active');
+      // clear input
+      container.find('.search-input').val('');
+      // clear and hide result container when we press close
+      container.find('.result-container').fadeOut(100, function(){$(this).empty();});
+   }
+}
---
+++ public/js/finance.js
@@ -0,0 +1,9 @@
+/**
+ * Created by QXT on 2017/7/7.
+ */
+
+$(function () {
+   //   tab
+   tab('.list-li', '.finance-sec');
+
+});
