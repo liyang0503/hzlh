e0839d25d1f8ff22b835b231f68c55fbdd1a1c64
liyang0503 <liyang0503@126.com>
2017-07-25 18:15:54 +0800

    个人中心

--- public/js/equity.js
+++ public/js/equity.js
@@ -3,6 +3,9 @@
  */
 
 $(function () {
+   //发现动效
+   // found();
+
    //   精品路演
    $('.qua-num').each(function () {
       var s1 = $(this).find('.s1').text();
@@ -23,19 +26,33 @@ $(function () {
 
 });
 
+//动效
+function found() {
+   $('.poster-item').each(function () {
+      if ($(this).css('zIndex') == 0) {
+         $(this).find('dl').css({transform: 'scale(0.85)'});
+      } else {
+         $(this).find('dl').css({transform: 'scale(1)'});
+      }
+   });
+   setTimeout(found, 3000);
+}
+
 //手机搜索
-function searchToggle(obj, e){
+function searchToggle(obj, e) {
    var container = $(obj).closest('.search-wrapper');
 
-   if(!container.hasClass('active')){
+   if (!container.hasClass('active')) {
       container.addClass('active');
       e.preventDefault();
    }
-   else if(container.hasClass('active') && $(obj).closest('.input-holder').length == 0){
+   else if (container.hasClass('active') && $(obj).closest('.input-holder').length == 0) {
       container.removeClass('active');
       // clear input
       container.find('.search-input').val('');
       // clear and hide result container when we press close
-      container.find('.result-container').fadeOut(100, function(){$(this).empty();});
+      container.find('.result-container').fadeOut(100, function () {
+         $(this).empty();
+      });
    }
 }
